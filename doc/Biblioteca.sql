8f1713bb53c55e2a2aa921b1887fc5ea71b72078
MaelSantos <39337708+maelsantos@users.noreply.github.com>
2019-07-13 14:47:24 -0300

    Implementação do Migration

--- doc/Biblioteca.sql
+++ doc/Biblioteca.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 4.9.0.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 17/06/2019 às 04:39
--- Versão do servidor: 10.1.38-MariaDB
--- Versão do PHP: 7.3.4
+-- Tempo de geração: 13-Jul-2019 às 19:46
+-- Versão do servidor: 10.3.16-MariaDB
+-- versão do PHP: 7.3.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -25,7 +25,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `Aluga`
+-- Estrutura da tabela `alembic_version`
+--
+
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `Aluga`
 --
 
 CREATE TABLE `Aluga` (
@@ -38,19 +48,19 @@ CREATE TABLE `Aluga` (
   `id_funcionario` int(11) NOT NULL,
   `id_cliente` int(11) NOT NULL,
   `id_livro` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ;
 
 --
--- Despejando dados para a tabela `Aluga`
+-- Extraindo dados da tabela `Aluga`
 --
 
 INSERT INTO `Aluga` (`id`, `data_locacao`, `data_devolucao`, `data_devolvido`, `diaria`, `ativo`, `id_funcionario`, `id_cliente`, `id_livro`) VALUES
-(1, '2019-06-13', '2019-06-20', NULL, 10, 1, 2, 1, 1);
+(1, '2019-07-11', '2019-07-10', '2019-07-11', 10, 1, 1, 4, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `Cliente`
+-- Estrutura da tabela `Cliente`
 --
 
 CREATE TABLE `Cliente` (
@@ -60,16 +70,17 @@ CREATE TABLE `Cliente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Despejando dados para a tabela `Cliente`
+-- Extraindo dados da tabela `Cliente`
 --
 
 INSERT INTO `Cliente` (`id`, `cpf`, `telefone`) VALUES
-(1, '075.510.743-84', '+5588981169372');
+(3, '23432424', '32421412'),
+(4, '433241', 'fds3242');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `Conta`
+-- Estrutura da tabela `Conta`
 --
 
 CREATE TABLE `Conta` (
@@ -81,19 +92,19 @@ CREATE TABLE `Conta` (
   `valor_total` float DEFAULT NULL,
   `ativo` tinyint(1) DEFAULT NULL,
   `id_aluga` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ;
 
 --
--- Despejando dados para a tabela `Conta`
+-- Extraindo dados da tabela `Conta`
 --
 
 INSERT INTO `Conta` (`id`, `data_efetuada`, `data_pagamento`, `data_paga`, `multa`, `valor_total`, `ativo`, `id_aluga`) VALUES
-(1, '2019-06-13', '2019-06-20', NULL, 0, 70, 1, 1);
+(1, '2019-07-01', '2019-07-10', '2019-07-11', 0, 10101000000000, 1, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `Funcionario`
+-- Estrutura da tabela `Funcionario`
 --
 
 CREATE TABLE `Funcionario` (
@@ -102,17 +113,16 @@ CREATE TABLE `Funcionario` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Despejando dados para a tabela `Funcionario`
+-- Extraindo dados da tabela `Funcionario`
 --
 
 INSERT INTO `Funcionario` (`id`, `cargo`) VALUES
-(2, 'admin'),
-(3, 'Atendente');
+(1, 'admin');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `Livro`
+-- Estrutura da tabela `Livro`
 --
 
 CREATE TABLE `Livro` (
@@ -125,19 +135,22 @@ CREATE TABLE `Livro` (
   `quantidade` int(11) NOT NULL,
   `disponivel` int(11) NOT NULL,
   `ativo` tinyint(1) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ;
 
 --
--- Despejando dados para a tabela `Livro`
+-- Extraindo dados da tabela `Livro`
 --
 
 INSERT INTO `Livro` (`id`, `autor`, `titulo`, `ano`, `editora`, `codigo`, `quantidade`, `disponivel`, `ativo`) VALUES
-(1, 'Teste', 'Teste', 2019, 'Teste', '000-9', 100, 100, 1);
+(1, 'Euzinho', 'Os que não foram', 2019, 'Sravá', '1000', 100, 100, 1),
+(2, 'jlksajdlk', 'sdjl', 1920, 'askdçls', '10920', 88, 88, 1),
+(3, 'ddjflka', 'oiueiwuoq', 98, 'akjsd', '0219jl', 300, 300, 1),
+(4, 'jlkjdask', 'wqelj', 9089, 'dsaoai', '0921ijk', 109, 109, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `Reserva`
+-- Estrutura da tabela `Reserva`
 --
 
 CREATE TABLE `Reserva` (
@@ -147,20 +160,21 @@ CREATE TABLE `Reserva` (
   `ativo` tinyint(1) DEFAULT NULL,
   `id_cliente` int(11) NOT NULL,
   `id_livro` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ;
 
 --
--- Despejando dados para a tabela `Reserva`
+-- Extraindo dados da tabela `Reserva`
 --
 
 INSERT INTO `Reserva` (`id`, `data_reserva`, `data_retirada`, `ativo`, `id_cliente`, `id_livro`) VALUES
-(2, '2019-06-15', '2019-06-22', 1, 1, 1),
-(3, '2019-06-16', '2019-06-23', 0, 1, 1);
+(1, '2019-07-11', '2019-07-18', 1, 4, 1),
+(2, '2019-07-11', '2019-07-18', 1, 4, 1),
+(3, '2019-07-11', '2019-07-18', 1, 4, 1);
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `Usuario`
+-- Estrutura da tabela `Usuario`
 --
 
 CREATE TABLE `Usuario` (
@@ -171,23 +185,29 @@ CREATE TABLE `Usuario` (
   `email` varchar(50) NOT NULL,
   `ativo` tinyint(1) DEFAULT NULL,
   `tipo` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ;
 
 --
--- Despejando dados para a tabela `Usuario`
+-- Extraindo dados da tabela `Usuario`
 --
 
 INSERT INTO `Usuario` (`id`, `nome`, `login`, `senha`, `email`, `ativo`, `tipo`) VALUES
-(1, 'Abimael Jonas Furtuoso dos Santos', 'mael', 'mael', 'mael', 1, 'Cliente'),
-(2, 'admin', 'admin', 'admin', 'admin', 1, 'Funcionario'),
-(3, 'Teste', 'Teste', 'teste', 'Teste', 1, 'Funcionario');
+(1, 'admin', 'admin', 'admin', 'admin', 1, 'Funcionario'),
+(3, 'gfds', 'mama', 'mama', 'dsfgfdssdfadmin', 1, 'Cliente'),
+(4, 'msdfgf', 'mau', 'maumau', 'admin32421', 1, 'Cliente');
 
 --
--- Índices de tabelas apagadas
+-- Índices para tabelas despejadas
 --
 
 --
--- Índices de tabela `Aluga`
+-- Índices para tabela `alembic_version`
+--
+ALTER TABLE `alembic_version`
+  ADD PRIMARY KEY (`version_num`);
+
+--
+-- Índices para tabela `Aluga`
 --
 ALTER TABLE `Aluga`
   ADD PRIMARY KEY (`id`),
@@ -196,34 +216,34 @@ ALTER TABLE `Aluga`
   ADD KEY `id_livro` (`id_livro`);
 
 --
--- Índices de tabela `Cliente`
+-- Índices para tabela `Cliente`
 --
 ALTER TABLE `Cliente`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `cpf` (`cpf`);
 
 --
--- Índices de tabela `Conta`
+-- Índices para tabela `Conta`
 --
 ALTER TABLE `Conta`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_aluga` (`id_aluga`);
 
 --
--- Índices de tabela `Funcionario`
+-- Índices para tabela `Funcionario`
 --
 ALTER TABLE `Funcionario`
   ADD PRIMARY KEY (`id`);
 
 --
--- Índices de tabela `Livro`
+-- Índices para tabela `Livro`
 --
 ALTER TABLE `Livro`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `codigo` (`codigo`);
 
 --
--- Índices de tabela `Reserva`
+-- Índices para tabela `Reserva`
 --
 ALTER TABLE `Reserva`
   ADD PRIMARY KEY (`id`),
@@ -231,7 +251,7 @@ ALTER TABLE `Reserva`
   ADD KEY `id_livro` (`id_livro`);
 
 --
--- Índices de tabela `Usuario`
+-- Índices para tabela `Usuario`
 --
 ALTER TABLE `Usuario`
   ADD PRIMARY KEY (`id`),
@@ -239,45 +259,45 @@ ALTER TABLE `Usuario`
   ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT de tabelas apagadas
+-- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
 -- AUTO_INCREMENT de tabela `Aluga`
 --
 ALTER TABLE `Aluga`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `Conta`
 --
 ALTER TABLE `Conta`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `Livro`
 --
 ALTER TABLE `Livro`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `Reserva`
 --
 ALTER TABLE `Reserva`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de tabela `Usuario`
 --
 ALTER TABLE `Usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Restrições para dumps de tabelas
+-- Restrições para despejos de tabelas
 --
 
 --
--- Restrições para tabelas `Aluga`
+-- Limitadores para a tabela `Aluga`
 --
 ALTER TABLE `Aluga`
   ADD CONSTRAINT `Aluga_ibfk_1` FOREIGN KEY (`id_funcionario`) REFERENCES `Funcionario` (`id`),
@@ -285,25 +305,25 @@ ALTER TABLE `Aluga`
   ADD CONSTRAINT `Aluga_ibfk_3` FOREIGN KEY (`id_livro`) REFERENCES `Livro` (`id`);
 
 --
--- Restrições para tabelas `Cliente`
+-- Limitadores para a tabela `Cliente`
 --
 ALTER TABLE `Cliente`
   ADD CONSTRAINT `Cliente_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Usuario` (`id`);
 
 --
--- Restrições para tabelas `Conta`
+-- Limitadores para a tabela `Conta`
 --
 ALTER TABLE `Conta`
   ADD CONSTRAINT `Conta_ibfk_1` FOREIGN KEY (`id_aluga`) REFERENCES `Aluga` (`id`);
 
 --
--- Restrições para tabelas `Funcionario`
+-- Limitadores para a tabela `Funcionario`
 --
 ALTER TABLE `Funcionario`
   ADD CONSTRAINT `Funcionario_ibfk_1` FOREIGN KEY (`id`) REFERENCES `Usuario` (`id`);
 
 --
--- Restrições para tabelas `Reserva`
+-- Limitadores para a tabela `Reserva`
 --
 ALTER TABLE `Reserva`
   ADD CONSTRAINT `Reserva_ibfk_1` FOREIGN KEY (`id_cliente`) REFERENCES `Cliente` (`id`),
