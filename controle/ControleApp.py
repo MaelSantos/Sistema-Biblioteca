91e30a580726d1ec79a08fdc3b648330c7e26ebb
MaelSantos <39337708+maelsantos@users.noreply.github.com>
2019-07-14 21:50:03 -0300

    Documento de Requisitos

--- controle/ControleApp.py
+++ controle/ControleApp.py
@@ -1,23 +1,20 @@
-import os
-from flask import Flask, send_from_directory
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 #Crição do App Principal
-app = Flask(__name__, template_folder="../view", static_folder="../estilo")
+app = Flask("Biblioteca", template_folder="../view", static_folder="../estilo")
 
 #Configurações do Banco
 app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:@localhost/Biblioteca?charset=utf8"
 app.config["SECRET_KEY"] = "babidi"
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
-# app.config["FLASK_APP"] = "Run.py"
-FLASK_APP = "app.py"
 
 db = SQLAlchemy(app)
 
 #Implementação da migration
 # COMANDOS UTILIZADOS NO TERMINAL
-# 1. é necessario exportar a aplicação usando: export FLASK_APP="controle/ControleApp.py""
+# 1. é necessario exportar a aplicação para ter acesso ao migration pelo terminal. Usando: export FLASK_APP="controle/ControleApp.py
 # 2. flask db init - Cria o repositorio de migração
 # 3. flask db migrate - gera uma migração inicial
 # 4. flask db upgrade - aplica a migração ao banco de dados
